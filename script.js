064b6aacc9e5f6f0e282eec9f28d246882e375db
guudewie <philippthiessen12@gmail.com>
2023-06-09 13:56:14 +0200

    write functions to draw in black, random, shadow and light

--- script.js
+++ script.js
@@ -1,16 +1,84 @@
 const grid = document.getElementById('grid')
 
-for (i=0; i < 256; i++) {
-    const singleGrid = document.createElement('div');
-    console.log(singleGrid)
-    singleGrid.addEventListener("mousemove", (e) => {
-        singleGrid.style.cssText = "background-color: red;"
-    });
-    grid.appendChild(singleGrid)
-    singleGrid.classList.add('singleGrid')
-};
 
 for (i=0; i < 256; i++) {
+    const singleGrid = document.createElement('div');
+    grid.appendChild(singleGrid)
+    singleGrid.classList.add('singleGrid')
+    drawLight(singleGrid)
+};
+
+
+
+function drawBlack (cell) {
+    cell.addEventListener("mouseenter", (e) => {
+        cell.style.cssText = "background-color: black"
+    });
+}
+
+function drawRandom (cell) {
+
+    cell.addEventListener("mouseenter", (e) => {
+        let a = Math.floor(Math.random()*255);
+        let b = Math.floor(Math.random()*255);
+        let c = Math.floor(Math.random()*255);
+
+        cell.style.cssText = `background-color: rgb(${a},${b},${c})`
+    });
+}
+
+function drawShadow (cell) {
+
+    cell.addEventListener("mouseenter", (e) => {
+
+        let rgbValue = window.getComputedStyle(cell, null).getPropertyValue("background-color")
+        let regEx = /(\d{1,3}), (\d{1,3}), (\d{1,3})/;
+
+        let rgbValuesArray = regEx.exec(rgbValue);
+
+        let rgbRed = rgbValuesArray[1]
+        let rgbGreen = rgbValuesArray[2]
+        let rgbBlue = rgbValuesArray[3]
+        let multiplier = 0.95
+
+        cell.style.cssText = `background-color: rgb(${rgbRed*multiplier},
+                                                    ${rgbGreen*multiplier},
+                                                    ${rgbBlue*multiplier})`
+    }); 
+}
+
+function drawLight (cell) {
+
+    cell.addEventListener("mouseenter", (e) => {
+
+        let rgbValue = window.getComputedStyle(cell, null).getPropertyValue("background-color")
+        let regEx = /(\d{1,3}), (\d{1,3}), (\d{1,3})/;
+
+        let rgbValuesArray = regEx.exec(rgbValue);
+        console.log(rgbValuesArray)
+        let rgbRed = rgbValuesArray[1]
+        let rgbGreen = rgbValuesArray[2]
+        let rgbBlue = rgbValuesArray[3]
+        let multiplier = 1.05
+
+        cell.style.cssText = `background-color: rgb(${(rgbRed)*multiplier},
+                                                    ${(rgbGreen)*multiplier},
+                                                    ${(rgbBlue)*multiplier})`
+    }); 
+}
+
+function erase (cell) {
+    cell.addEventListener("mouseenter", (e) => {
+        cell.style.cssText = "background-color: white"
+    });
+}
+
+
+
+
+
+for (i=0; i < 256; i++) {
+
 };
 
 
